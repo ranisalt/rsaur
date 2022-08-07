8b5cf94a17d80d7ee258af4b61bc6d9d867ad9d9
Ranieri Althoff <ranisalt@gmail.com>
2022-08-07 19:55:18 +0200

    Bump nvidia pkgrel on kernel headers update

--- nvidia-zen.sh
+++ nvidia-zen.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
 asp export nvidia
+
+NVIDIA_PKGREL=$(egrep -o '^pkgrel=.*' nvidia/PKGBUILD | cut -c8-)
+ZEN_PKGREL=$(pacman -Ss linux-zen-headers | cut -d' ' -f2 | cut -d'-' -f2)
+
 sed \
   -e '/pkgname=/ s/nvidia/nvidia-zen/' \
   -e '/depends=/ s/linux/linux-zen/' \
   -e '/pkgdesc=/ s/linux/linux-zen/' \
   -e '/kernver=/ s/linux/linux-zen/' \
+  -e "/pkgrel=/ s/$NVIDIA_PKGREL/$(($NVIDIA_PKGREL + $ZEN_PKGREL))/" \
   -i nvidia/PKGBUILD
-
-chown -R builduser nvidia
